b0170c9427e35b5e35a56ccdd048b24c49ea1e82
hanse <bluewood.truth@gmail.com>
2023-11-20 17:48:06 +0900

    setup dayjs and use now to fetch

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,15 +1,18 @@
 import { WeatherClient } from '$lib/weather-client';
+import dayjs from 'dayjs';
 import type { PageServerLoad } from './$types';
 
 WeatherClient.initialize(import.meta.env['VITE_WEATHER_API_KEY']);
 
 export const load: PageServerLoad = async () => {
-	const nowcast = await WeatherClient.getNowcast({
-		baseDate: '20231119',
-		baseTime: '2158',
-		nx: 60,
-		ny: 127
-	});
+  const now = dayjs().subtract(10, 'minute');
 
-	return { nowcast };
+  const nowcast = await WeatherClient.getNowcast({
+    baseDate: now.format('YYYYMMDD'),
+    baseTime: `${now.format('HH')}${now.minute() > 30 ? '30' : '00'}`,
+    nx: 60,
+    ny: 127,
+  });
+
+  return { nowcast };
 };
